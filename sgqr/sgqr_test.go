f971d109d49cb37381792f5f22561eca9c3d0164
Marcus Ong <devmarcusongyt@gmail.com>
2026-02-07 14:41:41 +0800

    add unit test

---
+++ sgqr/sgqr_test.go
@@ -0,0 +1,41 @@
+package sgqr
+
+import (
+	"testing"
+)
+
+func TestGeneratePaynowQrString(t *testing.T) {
+	options := PayNowQROptions{
+		MobileNumber:    "+6581010321",
+		Editable:        false,
+		Expiry:          "20251228",
+		Amount:          "10.50",
+		CompanyName:     "sgqr_test",
+		ReferenceNumber: "REF123",
+	}
+
+	result, err := GeneratePayNowQrString(options)
+	if err != nil {
+		t.Errorf("Error generating PayNow QR string: %v\n", err)
+	}
+	t.Logf("PayNow QR String: %s\n", result)
+}
+
+func TestGenerateSGQRString(t *testing.T) {
+	options := SGQROptions{
+		ReceiverType:             "uen",
+		MobileOrUENAccountNumber: "T11LL1111C",
+		Editable:                 false,
+		Expiry:                   "20251228",
+		Amount:                   "10.50",
+		SGQRID:                   "SGQR1234567890",
+		CompanyName:              "sgqr_test",
+		ReferenceNumber:          "REF123",
+	}
+
+	result, err := GenerateSGQRString(options)
+	if err != nil {
+		t.Errorf("Error generating SGQR string: %v\n", err)
+	}
+	t.Logf("SGQR String: %s\n", result)
+}
